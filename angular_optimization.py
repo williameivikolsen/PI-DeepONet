d0bd76f1a4e19ed9467cc0bd3fa9fac28da18b0e
William <williaeo@student.matnat.uio.no>
2026-08-31 13:33:59 +0200

    Prepared optimization run

--- angular_optimization.py
+++ angular_optimization.py
@@ -191,7 +191,7 @@ def objective(trial):
 
 if __name__ == "__main__":
     study = optuna.create_study(
-        storage=f"sqlite:///unnormalized_optimization.db",
+        storage=f"sqlite:///Q_normalized_optimization.db",
         study_name=f"{model_name}_tanh_lr_search",
         direction="minimize",
         sampler=optuna.samplers.GridSampler({"lr_config": list(LR_CANDIDATES)}),
